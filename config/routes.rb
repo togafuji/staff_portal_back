e81c8a51823c664fe32743baf446175de5428e4f
togafuji <yuichi.togsaki@vitalize.co.jp>
2024-10-30 23:32:37 +0900

    ログイン・ログアウトAPI実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             controllers: {
+               sessions: 'sessions'
+             }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # APIのベースURL
+  namespace :api do
+    namespace :v1 do
+      # ログイン用エンドポイント
+      devise_scope :user do
+        post 'login', to: 'sessions#create'
+        delete 'logout', to: 'sessions#destroy'
+      end
+    end
+  end
 end
