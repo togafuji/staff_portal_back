7fd25efab72bffd6e523a220b08c8449795f26f0
togafuji <yuichi.togsaki@vitalize.co.jp>
2025-01-01 14:09:30 +0900

    Add CI configuration for Rails

--- db/migrate/20241025144245_add_user_id_to_user_settings.rb
+++ db/migrate/20241025144245_add_user_id_to_user_settings.rb
@@ -2,8 +2,4 @@ class AddUserIdToUserSettings < ActiveRecord::Migration[7.0]
   def up
     change_column :user_settings, :user_id, :integer, null: false
   end
-
-  def down
-    change_column :user_settings, :user_id, :integer, null: true
-  end
 end
