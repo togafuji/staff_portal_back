1000b33a4e606bb9ff9a680e00ade1d01b8c5376
togafuji <yuichi.togsaki@vitalize.co.jp>
2025-01-01 14:31:11 +0900

    Add CI configuration for Rails

---
+++ db/migrate/20250101052643_add_id_to_users.rb
@@ -0,0 +1,5 @@
+class AddIdToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :user_settings, :user, null: false, foreign_key: true unless column_exists?(:user_settings, :user_id)
+  end
+end
